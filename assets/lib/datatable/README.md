831247291309493cb8fab0cca9e41051b97fc605
Pavel Halanin <Pavel.Innokentevich.Galanin@gmail.com>
2026-01-22 22:14:54 +0300

    main: sources added

---
+++ assets/lib/datatable/README.md
@@ -0,0 +1,3 @@
+https://cdn.datatables.net/1.13.4/css/dataTables.bootstrap5.min.css
+https://cdn.datatables.net/1.13.4/js/jquery.dataTables.min.js
+https://cdn.datatables.net/1.13.4/js/dataTables.bootstrap5.min.js
---
+++ script.js
@@ -0,0 +1,62 @@
+$(document).ready(function () {
+    $('#myTable').DataTable({
+        ajax: {
+            url: '/api/extrates/currencies/all.json',
+            type: 'GET',
+            dataSrc: 'data',
+            error: function (xhr, error, thrown) {
+                console.error(xhr);
+                document.getElementById('myTable').querySelector('tbody').innerHTML = `
+                    <div class="text-center">${xhr.status} ${xhr.statusText}</div>
+                `;
+                alert(`${xhr.status} ${xhr.statusText}`);
+            }
+        },
+        columns: [
+            {
+                title: 'ID',
+                data: 'Cur_Code'
+            },
+            {
+                title: 'Abbr',
+                data: 'Cur_Abbreviation'
+            },
+            {
+                title: 'Name',
+                data: 'Cur_Name_Eng'
+            },
+            {
+                title: 'Quot Name',
+                data: 'Cur_QuotName_Eng'
+            },
+            {
+                title: 'Multi Name',
+                data: 'Cur_Name_EngMulti'
+            },
+            {
+                title: 'Scale',
+                data: 'Cur_Scale'
+            },
+            {
+                title: 'Periodicity',
+                data: 'Cur_Periodicity'
+            },
+            {
+                title: 'Start',
+                data: 'Cur_DateStart'
+            },
+            {
+                title: 'End',
+                data: 'Cur_DateEnd'
+            },
+
+        ],
+        scrollY: "500px",
+        // scrollCollapse: true,
+        scroller: true,
+        paging: false,
+        language: {
+            url: "/assets/lib/datatable/language/ru.json"
+        }
+    });
+});
